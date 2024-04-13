686c66852b94d889586305c91260303afd7317f9
dsdunnmacbookpro <dsdunnmacbookpro@dsdunnmacbookpros-MacBook-Pro.local>
2024-04-13 16:08:25 -0600

    Fix dates

--- src/Shows.js
+++ src/Shows.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Shows = () => {
   const shows = [{
-    date: '2024-05-30',
+    date: '5/30',
     time: '8pm',
     image: null,
     venue: 'HQ Denver',
@@ -10,7 +10,7 @@ export const Shows = () => {
     url: 'https://fb.me/e/gHq2IcqUM'
   },
   {
-    date: '2024-08-11',
+    date: '8/11',
     time: '7pm',
     image: 'https://scontent-den2-1.xx.fbcdn.net/v/t39.30808-6/370374601_810282647452242_1402649304965174958_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=JyhpSoHj7fkAb4zr2FU&_nc_ht=scontent-den2-1.xx&oh=00_AfBW-4H30DJuyLi2ZPjrdZsEmvVIT7xY5nZNhlU2D4oTCA&oe=6618EDA2',
     venue: '1010 Workshop',
@@ -20,23 +20,16 @@ export const Shows = () => {
 
   return (
     <ul id="shows">
-      {shows.map(show => {
-        const showDate = new Date(show.date.split('-'));
-        const month = showDate.getMonth();
-        const day = showDate.getDay();
-
-        return (
+      {shows.map(show => (
           <li className="show flex light-blue">
-            <h2 className="show-date">
-              <span className="month">{month}</span>/<span className="day">{day}</span>
-            </h2>
+            <h2 className="show-date">{show.date}</h2>
             <div>
               <a href={show.url} target="_blank"><h2>{show.venue}</h2></a>
               <h3>{show.title}</h3>
             </div>
           </li>
-        );
-      })}
+        )
+      )}
     </ul>
   )
 }
